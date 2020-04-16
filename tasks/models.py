dcdea83f0bfd42e9b46c5883a380fdc9f4e62095
Pablo ( Paulo )  Lopez <pau_lo_lop_ez@protonmail.com>
2020-04-16 14:11:24 -0700

    created new template update_task.html and modified urls.py, models.py and views.py

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    # description = models.TextField()
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 
 def index(request):
     tasks = Task.objects.all()
-    context = {'tasks': tasks}
-    # return HttpResponse("Hello, world. You're at the tasks index.")
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'update_task.html', context)
